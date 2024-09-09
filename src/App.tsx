1cb98470ad92dc242986f818f24eda04efed0785
t-okuji <t.okuji34@gmail.com>
2024-09-09 11:24:02 +0900

    fix: Redirect behavior and add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GoogleCallback from "./pages/Callback/GoogleCallback";
 import Login from "./pages/Login";
 import UserInfo from "./pages/UserInfo";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth/google_auth/callback" element={<GoogleCallback />} />
-        <Route path="/me" element={<UserInfo />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/auth/google_auth/callback"
+            element={<GoogleCallback />}
+          />
+          <Route path="/me" element={<UserInfo />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Callback/GoogleCallback.tsx
+++ src/pages/Callback/GoogleCallback.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
@@ -5,27 +6,22 @@ function GoogleCallback() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
+
+  const KEY = `${
+    import.meta.env.VITE_API_URL
+  }/auth/google_auth/callback?code=${code}`;
+  const query = useQuery({ queryKey: [KEY], queryFn: () => fetch(KEY) });
+
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
-    if (code) {
-      const getInfo = async () => {
-        const response = await fetch(
-          `${
-            import.meta.env.VITE_API_URL
-          }/auth/google_auth/callback?code=${code}`,
-        );
-        if (!response.ok) {
-          throw new Error("API Error");
-        }
-        const result = await response.json();
-        navigate("/me");
-      };
-
-      getInfo();
+    if (query.isSuccess) {
+      navigate("/me", { replace: true });
     }
-  }, []);
+  }, [query.isSuccess]);
 
-  return <div>Google Callback Handling...</div>;
+  if (query.isLoading) {
+    return <div>Google Callback Handling...</div>;
+  }
 }
 
 export default GoogleCallback;
